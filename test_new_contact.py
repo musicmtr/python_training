179704c258cd1aa56aad49a304da4424e74bb796
musicmtr <music7mtr@mail.ru>
2020-12-06 18:03:20 +0300

    завершил

--- test_new_contact.py
+++ test_new_contact.py
@@ -25,7 +25,7 @@ class NewContact(unittest.TestCase):
         self.fill_contact_info(wd, Contact(firstname="testname", middlename="testmidl", lastname="testlas", nickname="testnicl", title="testtit", company="testcomp",
                                address="testadd - asd;m / asd/ 12", telhome="+7(864)151-424-77", telmob="+7(919)151-44-44", telwork="6161", fax="5616156",
                                email="test@mail.ru", mail2="test2@gmail.com", mail3="awd@mail.ru", homepage="wfwef@vk.ru", years1="1992", years2="1995",
-                               address2="wefwefawaefgwaeg\nawegawegaweg\nwaeg", phone2="WEFWF3", nots="wefawef",bday="1", aday="1", bmonth="May", amonth="June", photo=os.getcwd() + "/pik.jpeg"))
+                               address2="wefwefawaefgwaeg\nawegawegaweg\nwaeg", phone2="WEFWF3", nots="wefawef",bday="4", aday="4", bmonth="May", amonth="June", photo=os.getcwd() + "/pik.jpeg", newgroup="[none]"))
         self.back_home_page(wd)
         self.logout(wd)
 
@@ -37,7 +37,7 @@ class NewContact(unittest.TestCase):
         self.fill_contact_info(wd, Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                address="", telhome="", telmob="", telwork="", fax="",
                                email="", mail2="", mail3="", homepage="", years1="", years2="",
-                               address2="", phone2="", nots="", bday="", aday="", bmonth="", amonth="", photo=None))
+                               address2="", phone2="", nots="", bday="-", aday="-", bmonth="-", amonth="-", photo=None, newgroup="[none]"))
         self.back_home_page(wd)
         self.logout(wd)
 
@@ -99,26 +99,21 @@ class NewContact(unittest.TestCase):
         # даты, выпадающим списком
         wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_xpath("//option[@value='1']").click()
         wd.find_element_by_name("bmonth").click()
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_xpath("//option[@value='December']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(contact.years1)
         wd.find_element_by_name("aday").click()
         Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        wd.find_element_by_xpath("(//option[@value='1'])[2]").click()
         wd.find_element_by_name("amonth").click()
         Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_xpath("(//option[@value='June'])[2]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys(contact.years2)
         # принадлежность к группе
         wd.find_element_by_name("new_group").click()
-        Select(wd.find_element_by_name("new_group")).select_by_visible_text("asd")
-        wd.find_element_by_xpath("(//option[@value='16'])[3]").click()
+        Select(wd.find_element_by_name("new_group")).select_by_visible_text(contact.newgroup)
         # заполение вторичных полей
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
