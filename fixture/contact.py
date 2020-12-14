b52d07357f7b60c9efd123cb4a7f7a62e0545faa
musicmtr <music7mtr@mail.ru>
2020-12-14 21:20:56 +0300

    создан метод навигации, состояние рабочее, нужно добавить измнения групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -14,7 +14,7 @@ class ContactHelper:
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
-        self.app.open_home_page()
+        self.app.navigation.open_home_page()
 
     def fill_info(self, contact):
         # поля ФИО, НИК
