5f4f72febfc0d165b03edf94916db41b769d2277
musicmtr <music7mtr@mail.ru>
2020-12-26 18:31:17 +0300

    проверка на наличие группы перед удалением

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,11 @@ from model.group import Group
 
 
 def test_add_group(app):
-
     app.group.open_create_form()
     app.group.fill_group(Group(name="newgr1", header="newgr1hed", footer="nwe"))
     app.group.submit_create()
 
 def test_add_empty_group(app):
-
     app.group.open_create_form()
     app.group.fill_group(Group(name="", header="", footer=""))
     app.group.submit_create()
-
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,4 +1,10 @@
-def test_delete_first_group(app):
+from model.group import Group
 
+
+def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.open_create_form()
+        app.group.fill_group(Group(name="newgr1"))
+        app.group.submit_create()
     app.group.delete_first_group()
 
