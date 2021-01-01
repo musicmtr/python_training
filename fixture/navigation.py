f8de248340bf84adfe472a5019c752932f168274
musicmtr <music7mtr@mail.ru>
2021-01-01 15:29:01 +0300

    метод перехода и для навигации

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -5,12 +5,15 @@ class Navigation_Helper:
 
     def home_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/index.php")
+        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("Last name")) > 0):
+            wd.get("http://localhost/addressbook/index.php")
 
     def back_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home page").click()
+        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("Last name")) > 0):
+            wd.find_element_by_link_text("home page").click()
 
     def open_edit_page(self):
         wd = self.app.wd
-        wd.get("http://localhost/addressbook/edit.php")
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.get("http://localhost/addressbook/edit.php")
