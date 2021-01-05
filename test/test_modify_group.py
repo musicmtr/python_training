3b534cd9005171c63d2726e3dc319ebc50674020
qaokss <qa.okss@gmail.com>
2021-01-05 18:44:38 +0300

    сортировка списков, проблема с int

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,23 +6,27 @@ def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="newgr1"))
-    app.group.modify_first_group(Group(name="New group2"))
+    group = Group(name="New group2")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count() == 0:
-        app.group.create(Group(header="hea"))
-    app.group.modify_first_group(Group(header="new_header2"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_modify_group_header(app):
+#    old_groups = app.group.get_group_list()
+#    if app.group.count() == 0:
+#        app.group.create(Group(header="hea"))
+#    app.group.modify_first_group(Group(header="new_header2"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
 
-def test_modify_group_footer(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count() == 0:
-        app.group.create(Group(footer="fooo"))
-    app.group.modify_first_group(Group(footer="new_footer2"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_modify_group_footer(app):
+#    old_groups = app.group.get_group_list()
+#    if app.group.count() == 0:
+#        app.group.create(Group(footer="fooo"))
+#   app.group.modify_first_group(Group(footer="new_footer2"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
 
