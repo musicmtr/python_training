29bb8bc795c2a427330a0a3308f80cd92f933071
DrDany <ddv1551114@gmail.com>
2021-01-14 20:20:19 +0200

    add email check

--- test/test_phones.py
+++ test/test_phones.py
@@ -2,11 +2,11 @@ import re
 from random import randrange
 from model.contact import Contact
 
-
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#
+# def test_phones_on_home_page(app):
+#     contact_from_home_page = app.contact.get_contact_list()[0]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
@@ -19,8 +19,8 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.telwork == contact_from_edit_page.telwork
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
     assert contact_from_view_page.email == contact_from_edit_page.email
-    assert contact_from_view_page.email == contact_from_edit_page.email
-    assert contact_from_view_page.email == contact_from_edit_page.email
+    assert contact_from_view_page.mail2 == contact_from_edit_page.mail2
+    assert contact_from_view_page.mail3 == contact_from_edit_page.mail3
 
 
 def clear(s):
