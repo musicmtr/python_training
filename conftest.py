19d1045b853aa05bd385c9538f9048ece288bf1d
musicmtr <music7mtr@mail.ru>
2021-01-17 16:58:36 +0300

    фикстура БД

--- conftest.py
+++ conftest.py
@@ -5,24 +5,44 @@ import json
 import importlib
 import jsonpickle
 import os.path
+from fixture.db import DbFixture
 
 fixture = None
 target = None
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
 
 @pytest.fixture
 def app(request):
     global fixture
     global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, baseUrl=target["baseUrl"])
-        fixture.session.login(username=target['username'], password=target['password'], base_url=target["baseUrl"])
-    fixture.session.ensure_login(username=target['username'], password=target['password'])
+        fixture = Application(browser=browser, baseUrl=web_config["baseUrl"])
+        fixture.session.login(username=web_config['username'], password=web_config['password'], base_url=web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
     return fixture
+
+
+#фикстура соединения БД
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
