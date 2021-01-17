2d23ad322864576a2f562e270ab6ed0a40757298
musicmtr <music7mtr@mail.ru>
2021-01-17 12:12:48 +0300

    загрузка информации о тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        baseUrl = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, baseUrl=baseUrl)
-        fixture.session.login(username="admin", password="secret", base_url=baseUrl)
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseUrl=target["baseUrl"])
+        fixture.session.login(username=target['username'], password=target['password'], base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -28,6 +32,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
 
 
