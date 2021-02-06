fbdeec0b1273670c70f72edef7533098aa725946
DrDany <ddv1551114@gmail.com>
2021-02-06 13:49:12 +0200

    add ORM to check contct in group

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,13 +1,14 @@
 from fixture.orm import ORMFixture
 from model.group import Group
+from model.contact import Contact
 
 db = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 try:
-    l = db.get_contacts_not_in_group()
-    for item in l:
-        print(item)
-    print(len(l))
+    l = db.get_groups_not_in_contact(Contact(id="66"))
+    # for item in l:
+    #     print(item)
+    print(l)
 finally:
     pass #db.destroy()
 
