49df337e86595177e84a71058bbe30613708cc20
musicmtr <music7mtr@mail.ru>
2021-02-07 19:19:46 +0300

    ORM готов

--- test/test_contact_add_group.py
+++ test/test_contact_add_group.py
@@ -3,42 +3,24 @@ from model.contact import Contact
 import random
 
 
-
-def test_add_contact_in_group(app, db, json_contact, orm):
-    contacts = json_contact
-    list_connect = db.get_info_address_in_groups()
-    print("лист контакт:", "\n", list_connect, type(list_connect))
-    #проверка на наличие групп, если нет, создать
+def test_add_contact_in_some_group(app, orm, json_contact):
+    contact_inf = json_contact
     if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="newgr1"))
         app.group.create(Group(name="newgr2"))
         app.group.create(Group(name="newgr3"))
         app.contact.open_home_page()
-    #Проверка на наличие контактов, если нет, создать
-    if len(orm.get_contact_list()) == 0:
+    group = random.choice(orm.get_group_list())
+
+    if len(orm.get_contact_list()) == 0 or len(orm.get_contacts_not_in_group(group)) == 0:
         app.contact.open_add_new()
-        app.contact.fill_form(contacts)
+        app.contact.fill_form(contact_inf)
         app.contact.save_created()
         app.contact.open_home_page()
-    #Собираем информацию о контактах и группах
-    list_contact = orm.get_contact_list()
-    list_group = orm.get_group_list()
-    for contact in list_contact:
-        if len(orm.get_groups_not_in_contact(contact)) > 0:
-            list_empty_group = orm.get_groups_not_in_contact(contact)
-            index = list_empty_group[0].id
-            break
-            return contact, index
-        else:
-            app.group.create(Group(name="newemoty_group1111"))
-            list_empty_group = orm.get_groups_not_in_contact(contact)
-            index = list_empty_group[0].id
-            return index, contact
-            break
-    #выборка контакта для добавления
-    app.contact.open_home_page()
+    contact = random.choice(orm.get_contacts_not_in_group(group))
     app.contact.select_contact_by_id(contact.id)
-    #выборка группы и добавление
-    app.contact.edit_group_id(index)
-    new_list_connect = db.get_info_address_in_groups()
-    assert len(list_connect) + 1 == len(new_list_connect)
+    # выборка группы и добавление
+    app.contact.edit_group_id(group.id)
+    assert contact in orm.get_contacts_in_group(group)
+
+
