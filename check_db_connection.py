562d3da2f712add2a1971cfbc5e292c216cc772d
musicmtr <music7mtr@mail.ru>
2021-02-09 00:28:51 +0300

    orm попробовал написать с нуля еще допилил

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,13 @@
 from fixture.orm import ORMFixture
 from model.group import Group
 from model.contact import Contact
+import random
 
 db = ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 try:
-    #contact = random.choice(orm.get_free_contacts())
-    l = db.get_groups_in_contacts(Contact(id="13"))
+    #contact = random.choice(db.get_free_contacts())
+    l = db.get_group_list_have_contacts(Contact(id="21"))
     for item in l:
         print(item)
     print(len(l))
--- test/test_contact_add_group.py
+++ test/test_contact_add_group.py
@@ -3,33 +3,6 @@ from model.contact import Contact
 import random
 
 
-def test_add_contact_in_group(app, orm, json_contact):
-    contact_inf = json_contact
-    #Если группы нет, то добавляем
-    if len(orm.get_group_list()) == 0:
-        app.group.create(Group(name="newgr1"))
-        app.contact.open_home_page()
-    # Если нет контакта добавляем
-    if len(orm.get_contact_list()) == 0:
-        app.contact.open_add_new()
-        app.contact.fill_form(contact_inf)
-        app.contact.save_created()
-        app.contact.open_home_page()
-    # Берем свободного контакта
-    contact = random.choice(orm.get_free_contacts())
-    # Смотрим есть ли свободная группа для контакта, если нет создаем ищем свободную группу добавляем в нее контакт
-    if len(orm.get_group_list()) == len(orm.get_groups_in_contact(contact.id)):
-        app.group.create(Group(name="group_for_contact"))
-        app.contact.open_home_page()
-        group = orm.get_groups_not_in_contact(contact.id)
-        app.contact.add_contact_in_group_by_id(contact.id, group.id)
-        app.contact.open_home_page()
-    group = random.choice(orm.get_groups_not_in_contact(contact.id))
-    app.contact.add_contact_in_group_by_id(contact.id, group.id)
-    assert contact in orm.get_contacts_in_group(group)
-
-
-
 def test_add_contact_in_some_group(app, orm, json_contact):
     contact_inf = json_contact
     #Если группы нет, то добавляем
@@ -49,3 +22,25 @@ def test_add_contact_in_some_group(app, orm, json_contact):
     app.contact.add_contact_in_group_by_id(contact.id, group.id)
     list_contacts_in_group = orm.get_contacts_in_group(group)
     assert contact in list_contacts_in_group
+
+def test_add_contact_in_group(app, orm, json_contact):
+    contact_inf = json_contact
+    #Если группы нет, то добавляем
+    if len(orm.get_group_list()) == 0:
+        app.group.create(Group(name="newgr1"))
+        app.contact.open_home_page()
+    # Если свободного контакта от группы нет или нет контакта
+    if len(orm.get_free_contacts()) == 0:
+        app.contact.open_add_new()
+        app.contact.fill_form(contact_inf)
+        app.contact.save_created()
+        app.contact.open_home_page()
+    # Берем свободный контакт
+    contact = random.choice(orm.get_free_contacts())
+    # Смотрим есть ли свободная группа для контакта, если нет создаем ищем свободную группу добавляем в нее контакт
+    if len(orm.get_groups_not_in_contacts(contact)) == 0:
+        app.group.create(Group(name="group_for_contact"))
+        app.contact.open_home_page()
+    group = random.choice(orm.get_groups_not_in_contact(contact.id))
+    app.contact.add_contact_in_group_by_id(contact.id, group.id)
+    assert contact in orm.get_contacts_in_group(group)
